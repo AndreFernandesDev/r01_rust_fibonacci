6f44d2f261b07b3113cbca4dec1997d64398684e
Andre <andre.fernandesdev@gmail.com>
2024-05-22 20:52:53 +0100

    final result

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 This is a mini project used to solve the fibonnaci challenge.
 
 The easiest way to run it on your local system is using 'cargo run'.
+
+---
+
+### Disclaimer:
+
+This solution is not the greatest but it works fine according to the requirements.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Fibonacci Challenge");
+    println!("------------------------");
+
+    let mut input = String::new();
+
+    println!("Please insert your number:");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let input: u8 = input.trim().parse().expect("Please insert a number");
+
+    let mut a = 0;
+    let mut b = 0;
+
+    let nth = loop {
+        let c = a;
+
+        if a == 0 {
+            a += 1;
+        } else if b == 0 {
+            b += 1;
+        } else {
+            a = a + b;
+            b = c;
+        };
+
+        if a > input {
+            break c;
+        }
+    };
+
+    println!("Output: {nth}");
 }
